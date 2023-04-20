7dd528974235801dc775f006c43c4f10d41d3948
Matt McKeever <mmckeever23@gmail.com>
2023-04-20 08:29:47 -0500

    Removed roll button

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 import random
 
 root = Tk()
-root.geometry("200x275")
+root.geometry("200x250")
 root.configure(background='black') 
 
 def roll_dice():
@@ -25,20 +25,20 @@ my_frame.focus_set()
 
 my_frame.bind('<Key>', key_pressed)
 
-title = Label(my_frame, text='PPR\nRoller', font=('Helvetica', 20), fg='white', background="black")
+title = Label(my_frame, text='Pocket Pennant Run\nQuick Dice Roller', font=('Helvetica', 15), fg='white', background="black")
 title.grid(row=0, column=0, pady=10)
 
 dice_label1 = Label(my_frame, text=my_dice[0], font=('Helvetica', 25), fg = "red", background="black") 
 dice_label1.grid(row=1, column=0)
 
 dice_label2 = Label(my_frame, text=my_dice[1], font=('Helvetica', 25), fg = "yellow", background="black") 
-dice_label2.grid(row=3, column=0)
+dice_label2.grid(row=2, column=0)
 
 dice_label3 = Label(my_frame, text=my_dice[2], font=('Helvetica', 25), fg = "blue", background="black") 
-dice_label3.grid(row=5, column=0)
+dice_label3.grid(row=3, column=0)
 
-my_button = Button(root, text="Roll Dice", command = roll_dice, font=('Helvetica', 10), bg='white', activebackground='green')
-my_button.pack(pady=15)
+title = Label(my_frame, text='Press any key to roll', font=('Helvetica', 10), fg='white', background="black")
+title.grid(row=4, column=0, pady=10)
 
 roll_dice()
 
