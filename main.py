0e03fb617eb3555aef3431768a3c700a6f037961
Matt McKeever <mmckeever23@gmail.com>
2023-05-09 15:28:23 -0500

    Added rare sound fx

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 import winsound
 
 root = Tk()
-root.geometry("190x370")
+root.geometry("190x360")
 root.title("PPR")
 root.configure(background='black') 
 root.attributes("-topmost", True)
@@ -13,8 +13,8 @@ root.attributes("-topmost", True)
 sound_choice = tk.BooleanVar(value="False")
 
 def key_pressed(event):
-    roll_dice()
-    play_sound()
+    if event.keysym == 'space':
+        roll_dice()
 
 def sound_toggle():
         sound_choice.set(not sound_choice.get())
@@ -23,14 +23,36 @@ def sound_toggle():
         else:
             button.configure(text="Sound Off")
 
-def play_sound():
+def play_roll_sound():
     if sound_choice.get() == True:
         winsound.PlaySound("media/roll.wav", winsound.SND_ASYNC)
+
+def play_rare_sound():
+    if sound_choice.get() == True:
+        winsound.PlaySound("media/rare.wav", winsound.SND_ASYNC)
 
 def roll_dice(): 
     d1 = random.SystemRandom().randint(1,12)
     d2 = random.SystemRandom().randint(1,12)
     d3 = random.SystemRandom().randint(1,12)
+
+    if d1 == d2 and d2 == d3:
+        red_die.configure(image=gold, bd=0)
+        yellow_die.configure(image=gold, bd=0)
+        blue_die.configure(image=gold, bd=0)
+        dice_label1.configure(background="#726000")
+        dice_label2.configure(fg="white", background="#726000")
+        dice_label3.configure(background="#726000")
+        play_rare_sound()
+    else:
+        red_die.configure(image=red, bd=0)
+        yellow_die.configure(image=yellow, bd=0)
+        blue_die.configure(image=blue, bd=0)
+        dice_label1.configure(background="#FF5757")
+        dice_label2.configure(fg="black", background="#FFDE59")
+        dice_label3.configure(background="#5271FF")
+        play_roll_sound()
+
     if d1 > 9:
         dice_label1.place(x=81, y=86)
     else:
@@ -47,21 +69,6 @@ def roll_dice():
     dice_label1.config(text=d1)
     dice_label2.config(text=d2)
     dice_label3.config(text=d3)
-    
-    if d1 == d2 and d2 == d3:
-        red_die.configure(image=gold, bd=0)
-        yellow_die.configure(image=gold, bd=0)
-        blue_die.configure(image=gold, bd=0)
-        dice_label1.configure(background="#726000")
-        dice_label2.configure(fg="white", background="#726000")
-        dice_label3.configure(background="#726000")
-    else:
-        red_die.configure(image=red, bd=0)
-        yellow_die.configure(image=yellow, bd=0)
-        blue_die.configure(image=blue, bd=0)
-        dice_label1.configure(background="#FF5757")
-        dice_label2.configure(fg="black", background="#FFDE59")
-        dice_label3.configure(background="#5271FF")
 
 my_dice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 red = ImageTk.PhotoImage(Image.open('media/red.png').resize((72, 72)))
@@ -88,10 +95,10 @@ dice_label1 = Label(root, text=my_dice[0], font=('Helvetica', 15, 'bold'), fg = 
 dice_label2 = Label(root, text=my_dice[1], font=('Helvetica', 15, 'bold'), fg = "black", background="#FFDE59", justify="center") 
 dice_label3 = Label(root, text=my_dice[2], font=('Helvetica', 15, 'bold'), fg = "white", background="#5271FF", justify="center") 
 
-title = Label(root, text='Press any key to roll', font=('Helvetica', 10), fg='white', background="black")
-title.place(x=33, y=295)
+title = Label(root, text='Press space bar to roll', font=('Helvetica', 10), fg='white', background="black", justify="center", anchor='center', pady=10).pack()
+
  
 button = Button(root, text="Sound Off", bg="black", fg="white", command=sound_toggle)
-button.place(x=64, y=330)
+button.place(x=64, y=320)
 
 root.mainloop()
