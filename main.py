9afa5d976e104360c28c732e791cd582a37b88f5
Matt McKeever <mmckeever23@gmail.com>
2023-05-15 14:38:55 -0500

    Changed to green die for rare play

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import tkinter as tk
 from PIL import Image, ImageTk
 import winsound
+import tkinter.font as font
 
 # Application Properties
 root = Tk()
@@ -11,6 +12,7 @@ root.title("PPR")
 root.iconbitmap('media/logo.ico')
 root.configure(background='black') 
 root.attributes("-topmost", True)
+segoe = font.Font(family='Segoe UI')
 
 # Functions
 def key_pressed(event):
@@ -41,12 +43,12 @@ def roll_dice():
     dice_label3.config(text=d3)
     
     if d1 == d2 and d2 == d3:
-        red_die.configure(image=gold, bd=0)
-        yellow_die.configure(image=gold, bd=0)
-        blue_die.configure(image=gold, bd=0)
-        dice_label1.configure(background="#726000")
-        dice_label2.configure(fg="white", background="#726000")
-        dice_label3.configure(background="#726000")
+        red_die.configure(image=green, bd=0)
+        yellow_die.configure(image=green, bd=0)
+        blue_die.configure(image=green, bd=0)
+        dice_label1.configure(background="#398845")
+        dice_label2.configure(fg="white", background="#398845")
+        dice_label3.configure(background="#398845")
         play_rare_sound()
     else:
         red_die.configure(image=red, bd=0)
@@ -60,15 +62,15 @@ def roll_dice():
     if d1 > 9:
         dice_label1.place(x=81, y=86)
     else:
-        dice_label1.place(x=86, y=86)
+        dice_label1.place(x=87, y=86)
     if d2 > 9:
         dice_label2.place(x=81, y=159)
     else:
-        dice_label2.place(x=86, y=159)
+        dice_label2.place(x=87, y=159)
     if d3 > 9:
         dice_label3.place(x=81, y=231)
     else:
-        dice_label3.place(x=86, y=231)
+        dice_label3.place(x=87, y=231)
 
 # Frame Properties
 my_frame = Frame(root, bg='black') 
@@ -82,25 +84,24 @@ my_dice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 red = ImageTk.PhotoImage(Image.open('media/red.png').resize((72, 72)))
 yellow = ImageTk.PhotoImage(Image.open('media/yellow.png').resize((72, 72)))
 blue = ImageTk.PhotoImage(Image.open('media/blue.png').resize((72, 72)))
-gold = ImageTk.PhotoImage(Image.open('media/gold.png').resize((72, 72)))
+green = ImageTk.PhotoImage(Image.open('media/green.png').resize((72, 72)))
 
 # Page Title
-title = Label(my_frame, text='Pocket Pennant Run\nQuick Dice Roller', font=('Helvetica', 13), fg='white', background="black")
+title = Label(my_frame, text='Pocket Pennant Run\nQuick Dice Roller', font=('segoe', 13), fg='white', background="black")
 title.grid(row=0, column=0, pady=10)
 
-# 
+# Dice and Dice Labels
 red_die = tk.Label(root, image=red, bd=0)
 red_die.pack()
 yellow_die = tk.Label(root, image=yellow, bd=0)
 yellow_die.pack()
 blue_die = tk.Label(root, image=blue, bd=0)
 blue_die.pack()
+dice_label1 = Label(root, text=my_dice[0], font=('segoe', 15, 'bold'), fg = "white", background="#FF5757", justify="center") 
+dice_label2 = Label(root, text=my_dice[1], font=('segoe', 15, 'bold'), fg = "black", background="#FFDE59", justify="center") 
+dice_label3 = Label(root, text=my_dice[2], font=('segoe', 15, 'bold'), fg = "white", background="#5271FF", justify="center") 
 
-dice_label1 = Label(root, text=my_dice[0], font=('Helvetica', 15, 'bold'), fg = "white", background="#FF5757", justify="center") 
-dice_label2 = Label(root, text=my_dice[1], font=('Helvetica', 15, 'bold'), fg = "black", background="#FFDE59", justify="center") 
-dice_label3 = Label(root, text=my_dice[2], font=('Helvetica', 15, 'bold'), fg = "white", background="#5271FF", justify="center") 
-
-title = Label(root, text='Press space bar to roll', font=('Helvetica', 10), fg='white', background="black", justify="center", anchor='center', pady=10).pack()
+title = Label(root, text='Press space bar to roll', font=('segoe', 10), fg='white', background="black", justify="center", anchor='center', pady=10).pack()
 
 button = Button(root, text="Sound Off", bg="black", fg="white", command=sound_toggle)
 button.place(x=64, y=320)
